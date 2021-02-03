5a867a18ccf82c4931070ca9951ede08bc535510
elenabarashkova <lena.barashkova23@gmail.com>
2021-02-03 18:45:44 +0300

    feat: function add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let reverseN = 0;
+    n = Math.abs(n);
+      while(n !== 0) {
+        let ost = n % 10;
+        reverseN = reverseN * 10 + ost;
+      n = (n - ost)/10;
+    }
+    return reverseN;
 }
