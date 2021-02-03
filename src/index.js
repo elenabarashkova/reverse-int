7cfb30197f51b4d0b61315a3154bd7235d53b1cc
elenabarashkova <lena.barashkova23@gmail.com>
2021-02-03 19:09:28 +0300

    feat: second solution add

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ module.exports = function reverse (n) {
     }
     return reverseN;
 }
+
+// second solution (longer 16ms vs 10ms)
+// module.exports = function reverse (n) {
+//     n = Math.abs(n);
+//     nInStr = n + '';
+//     nInStr = nInStr.split("").reverse().join("");
+//     reverseN = parseInt(nInStr, 10);
+//     return reverseN; 
+// }
